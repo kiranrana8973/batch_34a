9f02140d67f858521ba850bf413602fc37e5cf6f
Kiran Rana <kiranrana8973@gmail.com>
2025-04-27 12:55:14 +0545

    Added stateful

--- lib/view/first_view.dart
+++ lib/view/first_view.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/material.dart';
 
-class FirstView extends StatelessWidget {
+class FirstView extends StatefulWidget {
   const FirstView({super.key});
+
+  @override
+  State<FirstView> createState() => _FirstViewState();
+}
+
+class _FirstViewState extends State<FirstView> {
+  int first = 0;
+  int second = 0;
+  int result = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("First View"),
+        title: Text("First Viewsd"),
         centerTitle: true,
         backgroundColor: Colors.red,
         elevation: 0,
       ),
       body: Column(
         children: [
-          TextField(),
+          TextField(
+            onChanged: (value) {
+              first = int.parse(value);
+            },
+          ),
           SizedBox(height: 8),
-          TextField(),
+          TextField(
+            onChanged: (value) {
+              second = int.parse(value);
+            },
+          ),
           SizedBox(height: 8),
           SizedBox(
             width: double.infinity,
-            child: ElevatedButton(onPressed: () {}, child: Text("Add")),
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  result = first + second;
+                });
+              },
+              child: Text("Add"),
+            ),
           ),
           SizedBox(height: 8),
 
-          Text("Result : 0"),
+          Text("Result : $result"),
         ],
       ),
     );
