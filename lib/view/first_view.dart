b9fbdb99b19b2fc513c5691cf29b82b38acc35c2
Kiran Rana <kiranrana8973@gmail.com>
2025-04-27 12:37:44 +0545

    Added column

--- lib/view/first_view.dart
+++ lib/view/first_view.dart
@@ -10,8 +10,23 @@ class FirstView extends StatelessWidget {
         title: Text("First View"),
         centerTitle: true,
         backgroundColor: Colors.red,
+        elevation: 0,
       ),
-      body: Text("Hello worsdfsdfld"),
+      body: Column(
+        children: [
+          TextField(),
+          SizedBox(height: 8),
+          TextField(),
+          SizedBox(height: 8),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(onPressed: () {}, child: Text("Add")),
+          ),
+          SizedBox(height: 8),
+
+          Text("Result : 0"),
+        ],
+      ),
     );
   }
 }
